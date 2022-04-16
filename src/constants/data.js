94de3c5c5a7f8620eedaaa30edb0bd22b65d455b
loidev <nhoxbaycao@gmail.com>
2022-04-16 12:54:51 +0700

    add data

---
+++ src/constants/data.js
@@ -0,0 +1,86 @@
+import {IMAGES} from './shared';
+import {COLORS} from './theme';
+
+export const SHOES_DATA = [
+  {
+    id: '1',
+    title: 'Shoe Nike One',
+    author: "Man's  Shoes",
+    price: '$350.000',
+    picture: IMAGES.shoes1,
+    colors: [COLORS.black, COLORS.orange, COLORS.green],
+  },
+  {
+    id: '2',
+    title: 'Shoe Nike Two',
+    author: "Man's  Shoes",
+    price: '$350.000',
+    picture: IMAGES.shoes2,
+    colors: [COLORS.black, COLORS.orange, COLORS.green],
+  },
+  {
+    id: '3',
+    title: 'Shoe Nike Three',
+    author: "Man's  Shoes",
+    price: '$350.000',
+    picture: IMAGES.shoes3,
+    colors: [COLORS.black, COLORS.orange, COLORS.green],
+  },
+];
+
+export const CATEGORIES_DATA = [
+  {
+    id: '1',
+    title: 'Nike',
+  },
+  {
+    id: '2',
+    title: 'Adidas',
+  },
+  {
+    id: '3',
+    title: 'Puma',
+  },
+  {
+    id: '4',
+    title: 'New Balance',
+  },
+];
+
+export const TYPES_DATA = [
+  {
+    id: '1',
+    title: 'Popular',
+  },
+  {
+    id: '2',
+    title: 'New Arrival',
+  },
+  {
+    id: '3',
+    title: 'Retailers',
+  },
+];
+
+export const SIZES_DATA = [
+  {
+    id: '1',
+    title: '10',
+  },
+  {
+    id: '2',
+    title: '10.5',
+  },
+  {
+    id: '3',
+    title: '11',
+  },
+  {
+    id: '4',
+    title: '11.5',
+  },
+  {
+    id: '5',
+    title: '12',
+  },
+];
---
+++ src/constants/shared.js
@@ -0,0 +1,23 @@
+export const ICONS = {
+  back: require('../assets/icons/back.png'),
+  carbon_add: require('../assets/icons/carbon_add.png'),
+  eva_bookmark_fill: require('../assets/icons/eva_bookmark_fill.png'),
+  filter: require('../assets/icons/filter.png'),
+  master_card: require('../assets/icons/master_card.png'),
+  menu_vertical: require('../assets/icons/menu_vertical.png'),
+  minus: require('../assets/icons/minus.png'),
+  nav_bar_search: require('../assets/icons/nav_bar_search.png'),
+  nav_bar: require('../assets/icons/nav_bar.png'),
+  new: require('../assets/icons/new.png'),
+  options: require('../assets/icons/options.png'),
+  trash: require('../assets/icons/trash.png'),
+  visa: require('../assets/icons/visa.png'),
+};
+
+export const IMAGES = {
+  background_login: require('../assets/images/background_login.png'),
+  payment_success: require('../assets/images/payment_success.png'),
+  shoes1: require('../assets/images/shoes1.png'),
+  shoes2: require('../assets/images/shoes2.png'),
+  shoes3: require('../assets/images/shoes3.png'),
+};
